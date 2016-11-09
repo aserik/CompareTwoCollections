420a660bea13490cad267ffc5ad99e8af9c69b28
Anna Serik <aserik@amtoss.com.ua>
2016-11-09 17:14:12 +0200

    add one more file with tests file

---
+++ ClassLibrary1/ClassLibrary1Tests/CollectionComparerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class CollectionComparerTests
+    {
+        [TestMethod()]
+        public void CompareEmptyAndNotEmpty_ShouldBeNotEqualTest()
+        {
+            var col1 = new[] { 1, 2, 3, 4, 5 };
+            var col2 = new int[] { };
+            var result = CollectionComparer.Compare(col1, col2);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void CompareTheSameElementsOrder_ShouldBeEqualTest()
+        {
+            var col1 = new[] { 1, 2, 3, 4, 5 };
+            var col2 = new int[] { 1, 2, 3, 4, 5 };
+            var result = CollectionComparer.Compare(col1, col2);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void CompareTheDifferentElementsOrder_ShouldNotBeEqualTest()
+        {
+            var col1 = new[] { 1, 2, 3, 4, 5 };
+            var col2 = new int[] { 1, 3, 2, 4, 5 };
+            var result = CollectionComparer.Compare(col1, col2);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void CompareTheDifferentElements_ShouldNotBeEqualTest()
+        {
+            var col1 = new[] { 1, 2, 3, 4, 5 };
+            var col2 = new int[] { 1, 3, 2, 4, 4 };
+            var result = CollectionComparer.Compare(col1, col2);
+            Assert.IsFalse(result);
+        }
+    }
+}
